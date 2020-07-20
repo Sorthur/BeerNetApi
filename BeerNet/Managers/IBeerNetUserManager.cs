a6bef3106e32f84f52a6536d20e2c2e15d9102c7
Mateusz Dejewski <matdejewski@gmail.com>
2020-07-20 23:55:44 +0200

    Add IBeerNetUserManager

---
+++ BeerNet/Managers/IBeerNetUserManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using BeerNet.Models;
+
+namespace BeerNet.Managers
+{
+    public interface IBeerNetUserManager
+    {
+        List<BeerNetUser> GetBeerNetUsers();
+        List<BeerNetUser> GetBeerNetUsers(string phraseInLogin);
+        BeerNetUser GetBeerNetUser(int id);
+        BeerNetUser GetBeerNetUser(string mail);
+        BeerNetUser GetBeerNetUserByLogin(string login);
+    }
+}
