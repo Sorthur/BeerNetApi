31afc4e61cec9b31d3a55617189e1037ea886b76
Mateusz Dejewski <matdejewski@gmail.com>
2020-07-20 21:28:16 +0200

    Add IBeerManager

---
+++ BeerNet/Managers/IBeerManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using BeerNet.Models;
+
+namespace BeerNet.Managers
+{
+    public interface IBeerManager
+    {
+        List<Beer> GetBeers();
+        List<Beer> GetBeers(string phraseInName);
+        Beer GetBeer(int id);
+        Beer GetBeer(string name);
+        Beer GetBeerWithHigherExtract(float givenExtract);
+        Beer GetBeerWithLowerExtract(float givenExtract);
+        Beer GetBeerWithExtractWithingRange(float moreThan, float lessThan);
+    }
+}
