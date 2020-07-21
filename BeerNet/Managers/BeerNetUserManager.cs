0fcfb65f2cb7d4f60757172b051292da99644cb2
Mateusz Dejewski <matdejewski@gmail.com>
2020-07-21 19:25:48 +0200

    Add class implementing IBeerNetUserManager

---
+++ BeerNet/Managers/BeerNetUserManager.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using BeerNet.Data;
+using BeerNet.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BeerNet.Managers
+{
+    public class BeerNetUserManager : IBeerNetUserManager
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public BeerNetUserManager(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public BeerNetUser GetBeerNetUser(string id)
+        {
+            return _dbContext.BeerNetUsers
+                .Include(b=>b.BeerRates)
+                .FirstOrDefault(b => b.Id == id);
+        }
+
+        public BeerNetUser GetBeerNetUserByMail(string mail)
+        {
+            return _dbContext.BeerNetUsers
+                .Include(b => b.BeerRates)
+                .FirstOrDefault(b => b.Email == mail);
+        }
+
+        public BeerNetUser GetBeerNetUserByLogin(string login)
+        {
+            return _dbContext.BeerNetUsers
+                .Include(b => b.BeerRates)
+                .FirstOrDefault(b => b.Login == login);
+        }
+
+        public List<BeerNetUser> GetBeerNetUsers()
+        {
+            return _dbContext.BeerNetUsers
+                .Include(b => b.BeerRates)
+                .ToList();
+        }
+
+        public List<BeerNetUser> GetBeerNetUsers(string loginPhrase)
+        {
+            return _dbContext.BeerNetUsers
+                .Where(b => b.Login.ToLower().Contains(loginPhrase.ToLower()))
+                .Include(b => b.BeerRates)
+                .ToList();
+        }
+    }
+}
