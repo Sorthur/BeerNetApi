9b86560fd75bedd6bd017789509413e96d391948
Mateusz Dejewski <matdejewski@gmail.com>
2020-07-21 15:25:50 +0200

    Add DbSets proporties in db context

--- BeerNet/Data/ApplicationDbContext.cs
+++ BeerNet/Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using BeerNet.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,6 +9,10 @@ namespace BeerNet.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<BeerNetUser> BeerNetUsers{ get; set; }
+        public DbSet<Beer> Beers { get; set; }
+        public DbSet<Brewery> Breweries { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
