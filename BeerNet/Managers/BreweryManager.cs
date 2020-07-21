542c45496da8fb188ed85cab9ade87a2604e3a1b
Mateusz Dejewski <matdejewski@gmail.com>
2020-07-21 18:41:00 +0200

    Add class implementing IBreweryManager

---
+++ BeerNet/Managers/BreweryManager.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using BeerNet.Data;
+using BeerNet.Models;
+using BeerNet.Models.Enums;
+using Microsoft.EntityFrameworkCore;
+
+namespace BeerNet.Managers
+{
+    public class BreweryManager :IBreweryManager
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public BreweryManager(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public List<Brewery> GetBreweries()
+        {
+            return _dbContext.Breweries
+                .Include(b=>b.Beers)
+                .ToList();
+        }
+
+        public List<Brewery> GetBreweries(string namePhrase)
+        {
+            return _dbContext.Breweries
+                .Where(b => b.Name.ToLower().Contains(namePhrase.ToLower()))
+                .Include(b => b.Beers)
+                .ToList();
+        }
+
+        public List<Brewery> GetBreweries(Country country)
+        {
+            return _dbContext.Breweries
+                .Where(b => b.Country == country)
+                .Include(b => b.Beers)
+                .ToList();
+        }
+
+        public Brewery GetBrewery(int id)
+        {
+            return _dbContext.Breweries
+                .Include(b => b.Beers)
+                .FirstOrDefault(b => b.Id == id);
+        }
+
+        public Brewery GetBrewery(string name)
+        {
+            return _dbContext.Breweries
+                .Include(b => b.Beers)
+                .FirstOrDefault(b => b.Name == name);
+        }
+    }
+}
