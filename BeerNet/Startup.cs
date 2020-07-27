3a0ec1b1e8e407971b5abf35261bbee73c3ebb1e
Mateusz Dejewski <matdejewski@gmail.com>
2020-07-27 16:10:14 +0200

    Registered BeerManager DI

--- BeerNet/Startup.cs
+++ BeerNet/Startup.cs
@@ -13,6 +13,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using BeerNet.Models;
+using BeerNet.Managers;
 
 namespace BeerNet
 {
@@ -35,6 +36,8 @@ namespace BeerNet
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
+
+            services.AddScoped<IBeerManager, BeerManager>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
