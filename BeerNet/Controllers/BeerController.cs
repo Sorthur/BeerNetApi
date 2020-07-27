a4633d95c52f77e860b26bfe99795fac97daa028
Mateusz Dejewski <matdejewski@gmail.com>
2020-07-27 16:11:43 +0200

    Add BeerController

---
+++ BeerNet/Controllers/BeerController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BeerNet.Data;
+using BeerNet.Managers;
+using BeerNet.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BeerNet.Controllers
+{
+    public class BeerController : Controller
+    {
+        private readonly IBeerManager _beerManager;
+
+        public BeerController(IBeerManager beerManager)
+        {
+            _beerManager = beerManager;
+        }
+        
+
+    }
+}
