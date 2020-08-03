53a58b09aeafd6acc026867104ddda9fbb11ee89
Mateusz Dejewski <matdejewski@gmail.com>
2020-08-03 18:32:16 +0200

    New methods in BeerController

--- BeerNet/Controllers/BeerController.cs
+++ BeerNet/Controllers/BeerController.cs
@@ -1,23 +1,65 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using BeerNet.Data;
 using BeerNet.Managers;
 using BeerNet.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BeerNet.Controllers
 {
+    [Route("[controller]/[action]")]
     public class BeerController : Controller
     {
+        private readonly UserManager<BeerNetUser> _userManager;
         private readonly IBeerManager _beerManager;
 
-        public BeerController(IBeerManager beerManager)
+        public BeerController(UserManager<BeerNetUser> userManager, IBeerManager beerManager)
         {
+            _userManager = userManager;
             _beerManager = beerManager;
         }
-        
 
+        [HttpGet()]
+        public IActionResult ToDo_AllBeers()
+        {
+            var beers = _beerManager.GetBeers();
+            return View();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult BeerDetails(int id)
+        {
+
+            var beer = _beerManager.GetBeer(id);
+            if (beer == null)
+            {
+                return View("AllBeers");
+            }
+            return View(beer);
+        }
+
+        [HttpGet]
+        public IActionResult BeerDetails(string beerName)
+        {
+            var beer = _beerManager.GetBeer(beerName);
+            if (beer == null)
+            {
+                return View("AllBeers");
+            }
+            return View(beer);
+        }
+
+        public IActionResult ToDo_AddRating(string BeerRateDescription, string ratingRange)
+        {
+            ratingRange = ratingRange.Replace('.', ',');
+            float rate = float.Parse(ratingRange);
+
+            BeerRateDescription.ToLower();
+            return RedirectToAction("BeerDetails", new { id = 1 });
+        }
     }
 }
