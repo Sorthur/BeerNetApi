f44b6c29160b3dd4ff64fcdd7488002ba216f74a
Mateusz Dejewski <matdejewski@gmail.com>
2020-08-10 16:43:49 +0200

    Add BreweryController and its View

---
+++ BeerNet/Controllers/BreweryController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using BeerNet.Data;
+using BeerNet.Managers;
+using BeerNet.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BeerNet.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class BreweryController : Controller
+    {
+        private readonly IBreweryManager _breweryManager;
+
+        public BreweryController(IBreweryManager breweryManager)
+        {
+            _breweryManager = breweryManager;
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult BreweryDetails(int id)
+        {
+            var brewery = _breweryManager.GetBrewery(id);
+            return View(brewery);
+        }
+
+        [HttpGet]
+        public IActionResult BreweryDetails(string name)
+        {
+            var brewery = _breweryManager.GetBrewery(name);
+            return View(brewery);
+        }
+    }
+}
--- BeerNet/Startup.cs
+++ BeerNet/Startup.cs
@@ -38,6 +38,7 @@ namespace BeerNet
             services.AddRazorPages();
 
             services.AddScoped<IBeerManager, BeerManager>();
+            services.AddScoped<IBreweryManager, BreweryManager>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
