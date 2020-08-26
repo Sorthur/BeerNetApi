aba8197f696cf14761257f138e73de374061b090
Mateusz Dejewski <matdejewski@gmail.com>
2020-08-26 15:56:08 +0200

    Add custom user page with user's ratings

---
+++ BeerNet/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BeerNet.Managers;
+using BeerNet.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BeerNet.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class UserController : Controller
+    {
+        private readonly IBeerNetUserManager _beerNetUserManager;
+        public UserController(IBeerNetUserManager beerNetUserManager)
+        {
+            _beerNetUserManager = beerNetUserManager;
+        }
+
+        [HttpGet("{login}")]
+        public IActionResult Index(string login)
+        {
+            var user = _beerNetUserManager.GetBeerNetUserByLogin(login);
+            return View(user);
+            // ToDo (user == null)
+        }
+    }
+}
