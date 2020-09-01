6214969374433ea28d978d099a99cb6a9eb6b9cf
Mateusz Dejewski <matdejewski@gmail.com>
2020-09-01 11:06:29 +0200

    Beer sorting in user's page

--- BeerNet/Controllers/UserController.cs
+++ BeerNet/Controllers/UserController.cs
@@ -13,20 +13,78 @@ namespace BeerNet.Controllers
     public class UserController : Controller
     {
         private readonly IBeerNetUserManager _beerNetUserManager;
+        public int d { get; set; }
         public UserController(IBeerNetUserManager beerNetUserManager)
         {
             _beerNetUserManager = beerNetUserManager;
         }
 
         [HttpGet("{login}")]
-        public IActionResult Index(string login)
+        public IActionResult Index(string login, string orderBy, bool sortAscending)
         {
             var user = _beerNetUserManager.GetBeerNetUserByLogin(login);
             if (user == null)
             {
                 return NotFound();
             }
+
+            user.BeerRates = SortBeerRates(user.BeerRates, orderBy, sortAscending);
+
+            if (sortAscending == false)
+            {
+                ViewBag.sortAscending = true;
+            }
+            else
+            {
+                ViewBag.sortAscending = false;
+            }
+
             return View(user);
+        }
+
+        private List<BeerRate> SortBeerRates(List<BeerRate> beerRates, string orderBy, bool sortAscending)
+        {
+            switch (orderBy)
+            {
+                case "Beer":
+                    if (sortAscending)
+                    {
+                        return beerRates.OrderBy(r => r.Beer.Name).ToList();
+                    }
+                    else
+                    {
+                        return beerRates.OrderByDescending(r => r.Beer.Name).ToList();
+                    }
+                case "Style":
+                    if (sortAscending)
+                    {
+                        return beerRates.OrderBy(r => r.Beer.Style).ToList();
+                    }
+                    else
+                    {
+                        return beerRates.OrderByDescending(r => r.Beer.Style).ToList();
+                    }
+                case "Score":
+                    if (sortAscending)
+                    {
+                        return beerRates.OrderBy(r => r.Rate).ToList();
+                    }
+                    else
+                    {
+                        return beerRates.OrderByDescending(r => r.Rate).ToList();
+                    }
+                case "Average":
+                    if (sortAscending)
+                    {
+                        return beerRates.OrderBy(r => r.Beer.AverageRating).ToList();
+                    }
+                    else
+                    {
+                        return beerRates.OrderByDescending(r => r.Beer.AverageRating).ToList();
+                    }
+                default:
+                    return beerRates;
+            }
         }
     }
 }
