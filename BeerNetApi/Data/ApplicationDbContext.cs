d1e68b7bf27d4ca21462931164a0c4298704d9bf
Mateusz Dejewski <matdejewski@gmail.com>
2020-10-05 17:40:49 +0200

    Add database context

---
+++ BeerNetApi/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using BeerNetApi.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BeerNetApi.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<BeerNetUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
