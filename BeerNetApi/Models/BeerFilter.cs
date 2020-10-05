22e3eaf4f00c09aef82e503eae98b3f80f6a98ea
Mateusz Dejewski <matdejewski@gmail.com>
2020-10-05 22:02:07 +0200

    Add BeerFilter model

---
+++ BeerNetApi/Models/BeerFilter.cs
@@ -0,0 +1,22 @@
+﻿using BeerNetApi.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BeerNetApi.Models 
+{
+    public class BeerFilter
+    {
+        public string BeerName { get; set; }
+        public string BreweryName { get; set; }
+        public Country? Country { get; set; }
+        public BeerStyle? BeerStyle { get; set; }
+        public float? MinExtract { get; set; }
+        public float? MaxExtract { get; set; }
+        public float? MinAbv { get; set; }
+        public float? MaxAbv { get; set; }
+        public int ObjectsToSkip { get; set; }
+        public int ObjectsToTake { get; set; }
+    }
+}
