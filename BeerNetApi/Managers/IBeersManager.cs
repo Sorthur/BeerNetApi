899e51d7174e01278b41608f5c769484b3af5ab5
Mateusz Dejewski <matdejewski@gmail.com>
2020-10-22 22:53:45 +0200

    Add manager interfaces

---
+++ BeerNetApi/Managers/IBeersManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using BeerNetApi.Models;
+using BeerNetApi.Models.Enums;
+
+namespace BeerNetApi.Managers
+{
+    public interface IBeersManager
+    {
+        int GetNumberOfBeers(BeerFilter beerFilter);
+        Beer GetBeer(int id);
+        List<Beer> GetBeers(BeerFilter beerFilter);
+        List<Beer> GetTopBeers(int numberOfBeers, int minNumberOfRatings);
+        void DodajListeLosowychPiw();
+    }
+}
