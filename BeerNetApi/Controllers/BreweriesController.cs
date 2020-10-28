6ecd1cd24b3289510e4766a99f5bda7d300b2b36
Mateusz Dejewski <matdejewski@gmail.com>
2020-10-28 13:04:58 +0100

    Add BreweriesController

---
+++ BeerNetApi/Controllers/BreweriesController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BeerNetApi.Managers;
+using BeerNetApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace BeerNetApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BreweriesController : ControllerBase
+    {
+        private readonly IBreweriesManager _breweriesManager;
+
+        public BreweriesController(IBreweriesManager breweriesManager)
+        {
+            _breweriesManager = breweriesManager;
+        }
+
+        /// <returns>
+        /// HTTP 200 with json with one brewery if it exists with (list of beers included) <br></br>
+        /// or HTTP 204 if beer with given id does not exist
+        /// </returns>
+        [HttpGet]
+        [Route("{breweryId}")]
+        public IActionResult Get(int breweryId)
+        {
+            var brewery = _breweriesManager.GetBrewery(breweryId);
+            if (brewery != null)
+            {
+                return Ok(brewery);
+            }
+            return NoContent();
+        }
+
+        /// <returns>
+        /// HTTP 200 with json with specific number of breweries for given filters (beers ignored) <br></br>
+        /// or HTTP 204 if no beer was found
+        /// </returns>
+        [HttpGet]
+        public IActionResult Get(BreweryFilter breweryFilter)
+        {
+            var breweries = _breweriesManager.GetBreweries(breweryFilter);
+            if (breweries != null)
+            {
+                return Ok(breweries);
+            }
+            return NoContent();
+        }
+    }
+}
