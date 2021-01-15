082fbaf0b1714eb74d8245ce4f74883f0eccc373
Mateusz Dejewski <matdejewski@gmail.com>
2021-01-15 14:51:12 +0100

    Add class - PostBeerRateModel

---
+++ BeerNetApi/Controllers/PostBeerRateModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BeerNetApi.Controllers
+{
+    public class PostBeerRateModel
+    {
+        [Required(ErrorMessage = "{0} is required")]
+        public string Description { get; set; }
+        [Required(ErrorMessage = "{0} is required")]
+        [Range(0.0, 5.0, ErrorMessage = "Value for {0} must be betwwen {1} and {2}")]
+        public float? Rate { get; set; }
+    }
+}
