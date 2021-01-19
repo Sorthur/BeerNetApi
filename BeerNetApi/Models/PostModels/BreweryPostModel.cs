390d28b4fa7576d5f95d798ec51e5cc32700d578
Mateusz Dejewski <matdejewski@gmail.com>
2021-01-19 23:14:10 +0100

    Add new class - BreweryPostModel

---
+++ BeerNetApi/Models/PostModels/BreweryPostModel.cs
@@ -0,0 +1,33 @@
+﻿using BeerNetApi.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BeerNetApi.Models.PostModels
+{
+    public class BreweryPostModel
+    {
+        [Required(ErrorMessage = "{0} is required")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "{0} is required")]
+        public string Description { get; set; }
+        [Required(ErrorMessage = "{0} is required")]
+        public Country? Country { get; set; }
+        [Required(ErrorMessage = "{0} is required")]
+        public byte[] Image { get; set; }
+
+        public static explicit operator Brewery(BreweryPostModel model)
+        {
+            return new Brewery
+            {
+                Beers = new List<Beer>(),
+                Country = model.Country.Value,
+                Description = model.Description,
+                Image = model.Image,
+                Name = model.Name
+            };
+        }
+    }
+}
