9558a27766a87a1cc18d25ff63c3c6843c12d9e0
Mateusz Dejewski <matdejewski@gmail.com>
2021-02-01 15:37:37 +0100

    Add swagger example classes

---
+++ BeerNetApi/SwaggerExamples/BeerRateExample.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+using BeerNetApi.Models;
+namespace BeerNetApi.SwaggerExamples
+{
+    /// <summary>
+    /// Class made only for swagger demonstration purposes
+    /// </summary>
+    public class BeerRateExample : BeerRate
+    {
+        [JsonIgnore]
+        public new Beer Beer { get; set; }
+    }
+}
