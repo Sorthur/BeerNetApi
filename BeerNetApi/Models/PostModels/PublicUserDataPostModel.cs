f1333fa8bae6151f7090d7a0c7b8acabf0ceee68
Mateusz Dejewski <matdejewski@gmail.com>
2021-03-08 21:38:50 +0100

    add new model - PublicUserDataPostModel

---
+++ BeerNetApi/Models/PostModels/PublicUserDataPostModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using BeerNetApi.Models.Enums;
+
+namespace BeerNetApi.Models.PostModels
+{
+    public class PublicUserDataPostModel
+    {
+        [Required]
+        public string UserId { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public Country? Country { get; set; }
+        public byte[] Image { get; set; }
+    }
+}
